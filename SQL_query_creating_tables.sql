8112a2ef71c5b6640334bd94b99a361c6203fd26
farahmh <61236001+farahmh@users.noreply.github.com>
2020-11-17 16:45:03 +0000

    Update SQL_query_creating_tables.sql

--- SQL_query_creating_tables.sql
+++ SQL_query_creating_tables.sql
@@ -3,6 +3,7 @@ USE plane_project;
 
 CREATE TABLE countries (
     country_code VARCHAR(3) PRIMARY KEY,
+    country_name VARCHAR(100) UNIQUE,
     additional_restrictions VARCHAR(15),
     visa_restrictions VARCHAR(30),
 );
@@ -20,11 +21,11 @@ CREATE TABLE passport_details (
 
 CREATE TABLE passenger_details (
     passenger_id INT IDENTITY PRIMARY KEY,
-    passport_id VARCHAR(30) NOT NULL REFERENCES passport_details(passport_id),
+    passport_id VARCHAR(50) NOT NULL REFERENCES passport_details(passport_id),
     first_name VARCHAR(15) NOT NULL,
     last_name VARCHAR(15) NOT NULL,
     dob DATE NOT NULL,
-    dependent_on VARCHAR(15) REFERENCES passenger_details(passenger_id),
+    dependent_on INT REFERENCES passenger_details(passenger_id),
     CONSTRAINT passenger_details CHECK
     (DATEDIFF(year, GETDATE(), dob) <= 12 OR dependent_on IS NOT NULL)
 );
@@ -35,6 +36,64 @@ CREATE TABLE airports(
     country_code VARCHAR(3) NOT NULL REFERENCES countries(country_code),
     longitude DECIMAL(9,6) NOT NULL,
     latitude DECIMAL(8,6) NOT NULL, 
-    
 
 );
+
+CREATE TABLE staff (
+    staff_id INT IDENTITY PRIMARY KEY,
+    first_name VARCHAR(15) NOT NULL,
+    last_name VARCHAR(15) NOT NULL,
+    dob DATE NOT NULL,
+    [role] VARCHAR(30)
+);
+
+CREATE TABLE credentials (
+    [user_id] INT IDENTITY PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    [password] VARCHAR(255) NOT NULL,
+    staff_id INT NOT NULL REFERENCES staff(staff_id),
+    [permissions] VARCHAR(5) NOT NULL
+);
+
+CREATE TABLE terminals (
+    terminal_id INT IDENTITY PRIMARY KEY,
+    airport_code VARCHAR(4) NOT NULL REFERENCES airports(airport_code),
+    terminal_capacity INT NOT NULL,
+    runway_size VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE planes (
+    plane_id INT IDENTITY PRIMARY KEY,
+    plane_type VARCHAR(10),
+    plane_capacity INT NOT NULL,
+    plane_size VARCHAR(4) NOT NULL,
+    fuel_capacity INT NOT NULL,
+    speed INT NOT NULL,
+    [weight] INT NOT NULL,
+    seats_available BIT NOT NULL,
+    fuel_per_km INT NOT NULL,
+    maintenance_date DATE
+);
+
+CREATE TABLE journey_details (
+    journey_id INT IDENTITY PRIMARY KEY,
+    plane_id INT REFERENCES planes(plane_id),
+    departure_time DATETIME,
+    arrival_time DATETIME,
+    departing_from VARCHAR(4) REFERENCES airports(airport_code),
+    arriving_to VARCHAR(4) REFERENCES airports(airport_code),
+
+);
+
+CREATE TABLE booking_details (
+    booking_id INT IDENTITY PRIMARY
+);
+
+CREATE TABLE ticket_details (
+    ticket_id INT IDENTITY PRIMARY KEY,
+    journey_id INT REFERENCES journey_details(journey_id),
+    seat_number VARCHAR(4),
+    terminal_id INT REFERENCES terminals(terminal_id),
+    passenger_id INT REFERENCES passenger_details(passenger_id),
+    booking_id INT REFERENCES booking
+)
