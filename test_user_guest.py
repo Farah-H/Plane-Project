d816982231626846216f44b117d746241ad42226
jatkin-wasti <jatkin-wasti@spartaglobal.com>
2020-11-18 15:00:31 +0000

    added tests for displaying a specific flights information in the user_guest file

--- test_user_guest.py
+++ test_user_guest.py
@@ -7,7 +7,10 @@ class TestGuest(unittest.TestCase):
     test_guest = Guest()
 
     def test_all_flights(self):
-        pass
+
 
     def test_display_flight(self):
-        pass
+        # Checking that some data is returned from the method when called with a valid flight reference
+        self.assertIsNotNone(self.test_guest.display_flight(8))
+        # Checking that False is returned when an invalid flight reference is passed
+        self.assertFalse(self.test_guest.display_flight("banana"))
