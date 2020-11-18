cf89d638ec1ef1745524b7d9173affc461f3c7b6
jatkin-wasti <jatkin-wasti@spartaglobal.com>
2020-11-18 12:21:36 +0000

    created new file with basic outline for testing the user_staff file

---
+++ test_user_staff.py
@@ -0,0 +1,22 @@
+from user_staff import Staff
+import unittest
+import pytest
+
+
+class TestStaff(unittest.TestCase):
+    test_staff = Staff()
+
+    def test_add_passenger(self):
+        pass
+
+    def test_display_flight(self):
+        pass
+
+    def test_display_passenger(self):
+        pass
+
+    def test_check_seats(self):
+        pass
+
+    def test_change_passenger(self):
+        pass
