9a78b11cd8072415c7f27e7c9285cd6be3e2187e
D3v <deviljin112@users.noreply.github.com>
2020-11-18 11:50:14 +0000

    Update user_guest.py

--- user_guest.py
+++ user_guest.py
@@ -12,14 +12,27 @@ class Guest(Connection):
         super().__init__("guest", "guest")
 
     def all_flights(self):
-        # Returns list
-        pass
+        data = []
+        query = "SELECT * FROM journey_details"
+        with self.cursor.execute(query):
+            row = self.cursor.fetchone()
+            while row:
+                data.append(row)
+                row = self.cursor.fetchone()
+        return data if len(data) > 0 else False
 
     # takes flight reference
     def display_flight(self, flight_ref):
-        # List
-        pass
+        data = None
+        query = f"SELECT * FROM journey_details WHERE journey_id = '{flight_ref}'"
+        with self.cursor.execute(query):
+            row = self.cursor.fetchall()
+            data = row
+        return data if data != None else False
 
+    ###########################
+    # PLACE HOLDER DO NOT USE #
+    ###########################
     def get_table(self, table_name):
         data = []
         query = f"SELECT * FROM {table_name}"
@@ -28,4 +41,4 @@ class Guest(Connection):
             while row:
                 data.append(row)
                 row = self.cursor.fetchone()
-        return data
+        return data if len(data) > 0 else False
