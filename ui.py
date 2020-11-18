73cb140671eecce9a69550a0535f97c8fe0ac54b
Jax <main@Jaxs-MacBook-Pro.local>
2020-11-18 12:30:45 +0000

    UI.py functionalities added

--- ui.py
+++ ui.py
@@ -11,7 +11,6 @@ class Flight:
 
 # the ui.py will interact with a user retrieve the required info
 # to accomplish a User Story 
-
 def body():
 
     # prompt the user to enter login details to access the booking system
@@ -38,6 +37,7 @@ def body():
 
     if choice_0 == 1:
         while True:
+            print("Welcome to the booking manager.")
             print("""\nPick an option:
                     1. Make a booking
                     2. Change the details of an existing booking
@@ -46,12 +46,17 @@ def body():
         
         # if the user inputs an invalid option the code runs again
         try:
-            choice_1 = int(input("--->  "))
+            choice_0_1= int(input("--->  "))
+            try: 
+                if choice_0_1 in range(1, 5):
+                    continue
+            except:
+                continue
         except:
             continue
     
-    # 
-    if choice_1 == 1:
+    # user is prompted to enter information required to make a booking
+    if choice_0_1 == 1:
         while True:
             print("\nIn order to make a booking, please answer the following questions: ")
             fname = input("Enter passenger first name:\n ").title()
@@ -62,8 +67,9 @@ def body():
             email_address = input("Enter passenger email address:\n ")
             telephone = input("Enter passenger telephone number:\n ")
             
-            choice_1_1 = input("YES or NO\nIs the passenger travelling with a minor? ").lower()
-            if choice_1_1 == "yes":
+            # accompanied minor or minors will also need to input details
+            choice_0_1_1 = input("YES or NO\nIs the passenger travelling with a minor? ").lower()
+            if choice_0_1_1 == "yes":
                 minor_number = input("How many minors are you travelling with:\n ").int()
                 for i in range(0, minor_number):
                     min_fname = input("Enter the minor first name:\n ").title()
@@ -71,9 +77,9 @@ def body():
                     min_p_number = input("Enter the minor passport number :\n ")
                     min_dob = input("Enter the minor date of birth as DD/MM/YYYY:\n ")
                     min_home_address = input("Enter the minor home address:\n ")
-            elif choice_1_1 =="no":
+            elif choice_0_1_1 =="no":
                 continue
-            departure_date = input("What day do you wish to depart?\n ")
+            departure_date = input("What day do you wish to depart?\n Format is DD/MM/YYYY: ")
             
             # list of cities and countries combined
             atlas = [] 
@@ -92,36 +98,95 @@ def body():
             else:
                 print("Please enter a valid destination location.")
 
-    elif choice_1 == 2:
+    elif choice_0_1 == 2:
         while True:
             print("""\nPick an option:
                     1. Change passenger information
                     2. Change flight details
-                    3. EXIT"""
+                    3. EXIT""")
 
         # if the user inputs an invalid option the code runs again
         try:
-            choice_1_2 = int(input("--->  "))
+            choice_0_1_2 = int(input("--->  "))
+            try: 
+                if choice_0_1_2 in range(1, 4):
+                    continue
+            except:
+                continue
         except:
             continue
-
-        if choice_1_2 == 1:
+        
+        # change passenger information, find out which information to change
+        if choice_0_1_2 == 1:
             while True:
                 print("""\nPick an option:
                         1. Change passport number
                         2. Change other passenger information
-                            3. EXIT"""
-    elif choice_1 == 3:
+                        3. EXIT""")
+            # if the user inputs an invalid option the code runs again
+            try:
+                choice_0_1_2_1= int(input("--->  "))
+                try: 
+                    if choice_0_1_2_1 in range(1, 4):
+                        continue
+                except:
+                    continue
+            except:
+                continue
+        # either the full name or the passport number remains the same
+        # we use sql to extract all the data on the passenger and UPDATE the relevant column
+        if choice_0_1_2_1 == 1:
             while True:
+                print("\nIn order to change passenger passport number please answer the following questions: ")
+                fname = input("Enter passenger first name:\n ").title()
+                lname = input("Enter passenger last name:\n ").title()
+                dob = input("Enter passenger date of birth as DD/MM/YYYY:\n ")
+                # we use sql to extract all the data on the passenger and UPDATE the relevant column
+
+        # change passemger personal information
+        elif choice_0_1_2_1 == 2:
+            while True:
+                print("\nIn order to change passenger personal information please answer the following question: ")
+                p_number = input("Enter passenger passport number:\n ")
+                # we use sql to extract all the data on the passenger and UPDATE the relevant column
+
+        # exit the interface
+        elif choice_0_1_2_1 == 3:
+            while True:
+                print("""\nEXIT""")
+
+ 
+        elif choice_0_1_2 == 2:
+            while True:
+                departure_date = input("What day do you wish to depart?\n Format is DD/MM/YYYY: ")
+                
+                # from_location existence is verified
+                from_location = input("Where will you be travelling from?\n")
+                if from_location in atlas:
+                    continue
+                else:
+                    print("Please enter a valid departure location.")
+                
+                # from_location existence is verified
+                to_location = input("Where will you be travelling to?\n")
+                if from_location in atlas:
+                    continue
+                else:
+                    print("Please enter a valid destination location.")
     
+    elif choice_0_1 == 3:
+        print("Thank you for your time, have a pleasant journey.")
+ 
+ 
+
+
+
     elif choice_1_2_1 == 1:
         while True:
         print("""\nPick an option:
                     1. Make a flight route
                     2. View flight routes
                     3. EXIT"""
-        
-choice_1_2_1
 
 
     elif choice_0 == 2:
@@ -131,9 +196,3 @@ choice_1_2_1
                     2. View flight routes
                     3. EXIT"""
         
-        # if the user inputs an invalid option the code runs again
-        try:
-            choice_1 = int(input("--->  "))
-        except:
-            continue
-    
