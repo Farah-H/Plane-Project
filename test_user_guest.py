aa81a1fe8ec2612ecc79617d0f7cbfd743752dc3
jatkin-wasti <jatkin-wasti@spartaglobal.com>
2020-11-18 14:53:02 +0000

    added base test file for user_guest.py

---
+++ test_user_guest.py
@@ -0,0 +1,13 @@
+from user_guest import Guest
+import unittest
+import pytest
+
+
+class TestGuest(unittest.TestCase):
+    test_guest = Guest()
+
+    def test_all_flights(self):
+        pass
+
+    def test_display_flight(self):
+        pass
